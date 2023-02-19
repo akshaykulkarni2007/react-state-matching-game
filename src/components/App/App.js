84f41dbda4af30b151d563f9baa6b9417573b132
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2023-02-19 14:49:24 +0530

    module 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
 
-import './App.css';
+import { createTiles } from '../../misc/utils'
 
-class App extends Component{
+import './App.css'
 
-  render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Turbo-Matcher
-      </header>
-        <OptionsPanel />
-        <Board />
-      }
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props)
 
-  }
+		this.state = {
+			numTiles: 35,
+			playing: false,
+			previousTileIndex: null,
+			tiles: [],
+			toBeCleared: null,
+		}
+	}
+
+	startGame(numTiles) {
+		this.setState((state) => ({
+			playing: true,
+			previousTileIndex: null,
+			toBeCleared: null,
+			tiles: createTiles(state.numTiles, this.handleTileClicked),
+		}))
+	}
+
+	render() {
+		const { numTiles, playing, previousTileIndex, tiles, toBeCleared } =
+			this.state
+
+		return (
+			<div className="App">
+				<header className="App-header">Turbo-Matcher</header>
+
+				<OptionsPanel
+					playing={playing}
+					numTiles={numTiles}
+					startGame={startGame}
+				/>
+				<Board numTiles={numTiles} tiles={tiles} />
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 import './Tile.css'
 
-const Tile = () => {
+const Tile = (props) => {
+	const dynamicColor =
+		props.selected || props.matched ? { backgroundColor: props.color } : null
 
-  return (
-    <div className='Tile'>
-    </div>
-  )
+	return (
+		<div className="Tile" style={dynamicColor}>
+			{props.selected || props.matched ? <props.svg /> : null}
+		</div>
+	)
 }
 
 export default Tile
